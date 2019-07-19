38e0f376f79e8e995117263c1943b349200124a5
Jairo Cruz <azaveliz@gmail.com>
2019-07-18 21:24:33 -0600

    Add Model, and persist with usertdefault

---
+++ TodoListApp/item.swift
@@ -0,0 +1,14 @@
+//
+//  item.swift
+//  TodoListApp
+//
+//  Created by MacBook Pro on 7/5/19.
+//  Copyright © 2019 MacBook Pro. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
